aee094e83df67c11adbdd037c94f08e3ed687324
Dan Firth <locallycompact@gmail.com>
2020-07-23 20:10:26 +0100

    typo

--- src/Development/Shake/Plus/Extended/Local.hs
+++ src/Development/Shake/Plus/Extended/Local.hs
@@ -3,6 +3,7 @@ module Development.Shake.Plus.Extended.Local where
 import Development.Shake.Plus
 import RIO
 import Path
+import Within
 
 class HasLocalOut r where
   localOut :: Lens' r (Path b Dir)
@@ -20,12 +21,12 @@ delocaliseOut x = ask >>= \r -> return $ (view localOut r </> x)
   let d = view localOut r
   ((toFilePath d <>) <$> xs) |%> (ract <=< stripProperPrefix d)
 
-(/%^>) :: (MonadReader r m, HasLocalOut r, MonadRules m) => FilePattern -> (Within Rel File -> RAction r ()) -> m ()
+(/%^>) :: (MonadReader r m, HasLocalOut r, MonadRules m) => FilePattern -> (Within Rel (Path Rel File) -> RAction r ()) -> m ()
 (/%^>) xs ract = ask >>= \r -> do
   let d = view localOut r
-  (toFilePath d <> xs) %> (ract <=< `asWithin` d)
+  (toFilePath d <> xs) %> (ract <=< (`asWithin` d))
 
-(/|%^>) :: (MonadReader r m, HasLocalOut r, MonadRules m) => [FilePattern] -> (Within Rel File -> RAction r ()) -> m ()
+(/|%^>) :: (MonadReader r m, HasLocalOut r, MonadRules m) => [FilePattern] -> (Within Rel (Path Rel File) -> RAction r ()) -> m ()
 (/|%^>) xs ract = ask >>= \r -> do
   let d = view localOut r
-  ((toFilePath d <>) <$> xs) |%> (ract <=< `asWithin` d)
+  ((toFilePath d <>) <$> xs) |%> (ract <=< (`asWithin` d))
