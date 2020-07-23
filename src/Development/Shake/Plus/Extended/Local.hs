92038c15162fe9854970ffb9f54a5503f9ffd283
Dan Firth <locallycompact@gmail.com>
2020-07-23 22:57:55 +0100

    Change to tuple

--- src/Development/Shake/Plus/Extended/Local.hs
+++ src/Development/Shake/Plus/Extended/Local.hs
@@ -8,25 +8,12 @@ import Within
 class HasLocalOut r where
   localOut :: Lens' r (Path b Dir)
 
-delocaliseOut :: (MonadReader r m, HasLocalOut r) => Path Rel t -> m (Path Rel t)
-delocaliseOut x = ask >>= \r -> return $ (view localOut r </> x)
-
-(/%>) :: (MonadReader r m, HasLocalOut r, MonadRules m) => FilePattern -> (Path Rel File -> RAction r ()) -> m ()
+(/%>) :: (MonadReader r m, HasLocalOut r, MonadRules m) => FilePattern -> ((Path Rel Dir, Path Rel File) -> RAction r ()) -> m ()
 (/%>) xs ract = ask >>= \r -> do
   let d = view localOut r
-  (toFilePath d <> xs) %> (ract <=< stripProperPrefix d)
+  (toFilePath d <> xs) %> ract (\x -> ract (d, x))
 
-(/|%>) :: (MonadReader r m, HasLocalOut r, MonadRules m) => [FilePattern] -> (Path Rel File -> RAction r ()) -> m ()
+(/|%>) :: (MonadReader r m, HasLocalOut r, MonadRules m) => [FilePattern] -> ((Path Rel Dir, Path Rel File) -> RAction r ()) -> m ()
 (/|%>) xs ract = ask >>= \r -> do
   let d = view localOut r
-  ((toFilePath d <>) <$> xs) |%> (ract <=< stripProperPrefix d)
-
-(/%^>) :: (MonadReader r m, HasLocalOut r, MonadRules m) => FilePattern -> (Within Rel (Path Rel File) -> RAction r ()) -> m ()
-(/%^>) xs ract = ask >>= \r -> do
-  let d = view localOut r
-  (toFilePath d <> xs) %> (ract <=< (`asWithin` d))
-
-(/|%^>) :: (MonadReader r m, HasLocalOut r, MonadRules m) => [FilePattern] -> (Within Rel (Path Rel File) -> RAction r ()) -> m ()
-(/|%^>) xs ract = ask >>= \r -> do
-  let d = view localOut r
-  ((toFilePath d <>) <$> xs) |%> (ract <=< (`asWithin` d))
+  ((toFilePath d <>) <$> xs) |%> (\x -> ract (d, x))
