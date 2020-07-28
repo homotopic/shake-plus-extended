3d7495cd9891743c21f5586783da9ec8e4a619b8
Dan Firth <locallycompact@gmail.com>
2020-07-28 19:55:19 +0100

    Move local.hs to filerules.hs

--- src/Development/Shake/Plus/Extended/FileRules.hs
+++ src/Development/Shake/Plus/Extended/FileRules.hs
@@ -7,7 +7,10 @@
 
 -}
 module Development.Shake.Plus.Extended.FileRules (
-  (%^>)
+  HasLocalOut(..)
+, (/%>)
+, (|/%>)
+, (%^>)
 , (|%^>)
 ) where
 
@@ -17,6 +20,21 @@ import           Development.Shake.FilePath
 import           Development.Shake.Plus
 import           RIO                         as R
 import           Within
+
+class HasLocalOut r where
+  localOutL :: Lens' r (Path Rel Dir)
+
+-- | Variant of `(%>)` that passes the local directory from the environment into the callback.
+(/%>) :: (MonadReader r m, HasLocalOut r, MonadRules m) => FilePattern -> ((Path Rel Dir, Path Rel File) -> RAction r ()) -> m ()
+(/%>) xs ract = ask >>= \r -> do
+  let d = view localOutL r
+  (toFilePath d <> xs) %> (stripProperPrefix d >=> \x -> ract (d, x))
+
+-- | Variant of `(|%>)` that passes the local directory from the environment into the callback.
+(/|%>) :: (MonadReader r m, HasLocalOut r, MonadRules m) => [FilePattern] -> ((Path Rel Dir, Path Rel File) -> RAction r ()) -> m ()
+(/|%>) xs ract = ask >>= \r -> do
+  let d = view localOutL r
+  ((toFilePath d <>) <$> xs) |%> (stripProperPrefix d >=> \x -> ract (d, x))
 
 -- | `Within` variant of `(%>)`, used to keep track of local directories.
 (%^>) :: (Partial, MonadReader r m, MonadRules m) => Within Rel FilePattern -> (Within Rel (Path Rel File) -> RAction r ()) -> m ()
