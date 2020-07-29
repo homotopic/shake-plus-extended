7fb4fcc61e0a066a51fd580c0a367eab133b0221
Dan Firth <locallycompact@gmail.com>
2020-07-29 21:19:15 +0100

    Add new runSimpleShakePlus

--- src/Development/Shake/Plus/Extended/Simple.hs
+++ src/Development/Shake/Plus/Extended/Simple.hs
@@ -8,17 +8,41 @@
 Shortcuts to run shake-plus with a simple environment.
 -}
 module Development.Shake.Plus.Extended.Simple (
- runSimpleShakePlus
+  SimpleSPlusEnv(..)
+, runLoggedShakePlus
+, runSimpleShakePlus
 ) where
 
 import           Control.Exception
 import           Development.Shake.Plus
+import           Development.Shake.Plus.Extended.FileRules
 import           RIO
 
+data SimpleSPlusEnv = SimpleSPlusEnv {
+  logFunc  :: LogFunc
+, localOut :: Path Rel Dir
+}
+
+instance HasLogFunc SimpleSPlusEnv where
+  logFuncL = lens logFunc (\x y -> x { logFunc = y} )
+
+instance HasLocalOut SimpleSPlusEnv where
+  localOutL = lens localOut (\x y -> x { localOut = y})
+
 -- | Run a `ShakePlus` with just a `LogFunc` in the environment that logs to stderr.
-runSimpleShakePlus :: MonadIO m => ShakePlus LogFunc a -> m ()
-runSimpleShakePlus m = do
+runLoggedShakePlus :: MonadIO m => ShakePlus LogFunc a -> m ()
+runLoggedShakePlus m = do
   lo <- logOptionsHandle stderr True
   (lf, dlf) <- newLogFunc (setLogMinLevel LevelInfo lo)
   liftIO $ shakeArgs shakeOptions $ void $ runShakePlus lf m
+  dlf
+
+
+-- | Run a `ShakePlus` with just a `SimpleSPlusEnv`.
+runSimpleShakePlus :: MonadIO m => Path Rel Dir -> ShakePlus SimpleSPlusEnv a -> m ()
+runSimpleShakePlus outputFolder m = do
+  lo <- logOptionsHandle stderr True
+  (lf, dlf) <- newLogFunc (setLogMinLevel LevelInfo lo)
+  let env = SimpleSPlusEnv lf outputFolder
+  liftIO $ shakeArgs shakeOptions $ void $ runShakePlus env m
   dlf
